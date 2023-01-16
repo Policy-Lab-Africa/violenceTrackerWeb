52b7f669f48be4498af93edd5c8f34b1cc108771
cremirdevio <josephajibodu@gmail.com>
2023-01-16 14:06:28 +0100

    chore: mobile hambugger implemented

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,15 @@
-import { Box, Button, chakra, Flex, Stack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  chakra,
+  Flex,
+  shouldForwardProp,
+  Stack,
+  Text,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { isValidMotionProp, motion, useMotionValue } from 'framer-motion';
 import { useState } from 'react';
 import { NextChakraImage } from '../Images/NextChakraImage';
 
@@ -50,7 +61,31 @@ const NavItem = ({ item }: { item: NavItemData }) => {
   );
 };
 
+const AnimatedBox = chakra(motion.div, {
+  /**
+   * Allow motion props and non-Chakra props to be forwarded.
+   */
+  shouldForwardProp: (prop) =>
+    isValidMotionProp(prop) || shouldForwardProp(prop),
+});
+
 export default function Header() {
+  const { isOpen, onToggle } = useDisclosure();
+  const barAVariant = {
+    open: { top: `50%`, rotate: `45deg` },
+    closed: { top: `25%`, rotate: `0deg` },
+  };
+
+  const barBVariant = {
+    open: { width: `0%` },
+    closed: { width: `50%` },
+  };
+
+  const barCVariant = {
+    open: { top: `50%`, rotate: `-45deg` },
+    closed: { top: `75%`, rotate: `0deg` },
+  };
+
   return (
     <Box bgColor={`surface`} width={`full`} fontSize={`1rem`}>
       {/* Main Nav Container */}
@@ -58,7 +93,8 @@ export default function Header() {
         width={`full`}
         justifyContent="space-between"
         alignItems="center"
-        px={`5rem`}
+        pl={[`0rem`, `5rem`]}
+        pr={[`1rem`, `5rem`]}
         height={`124px`}
       >
         {/* Logo */}
@@ -72,7 +108,7 @@ export default function Header() {
         </Flex>
 
         {/* Desktop Nav */}
-        <Flex>
+        <Flex display={[`none`, `flex`]}>
           <Stack direction={`row`} alignItems="center">
             {NAV_ITEM.map((item) => (
               <NavItem item={item} key={item.label} />
@@ -90,6 +126,43 @@ export default function Header() {
               Report Violence
             </Button>
           </Stack>
+        </Flex>
+
+        {/* Hamburger */}
+        <Flex
+          position={`relative`}
+          width={`48px`}
+          height={`48px`}
+          justifyContent={`center`}
+          onClick={onToggle}
+        >
+          <AnimatedBox
+            variants={barAVariant}
+            animate={isOpen ? barAVariant.open : barAVariant.closed}
+            position={`absolute`}
+            top={`25%`}
+            height={`2px`}
+            width={`80%`}
+            bgColor={`primary.500`}
+          />
+          <AnimatedBox
+            variants={barBVariant}
+            animate={isOpen ? barBVariant.open : barBVariant.closed}
+            position={`absolute`}
+            top={`50%`}
+            height={`2px`}
+            width={`50%`}
+            bgColor={`primary.500`}
+          />
+          <AnimatedBox
+            variants={barCVariant}
+            animate={isOpen ? barCVariant.open : barCVariant.closed}
+            position={`absolute`}
+            top={`75%`}
+            height={`2px`}
+            width={`80%`}
+            bgColor={`primary.500`}
+          />
         </Flex>
       </Flex>
     </Box>
