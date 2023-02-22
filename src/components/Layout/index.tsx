b4f6be3711073e48bf125c210715bcd0c72f55dc
cremirdevio <josephajibodu@gmail.com>
2023-02-22 23:07:37 +0100

    feat: pwa feature completed

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -13,6 +13,34 @@ export default function Layout({ children }: { children: JSX.Element }) {
           content="Report and Share voilence incidence."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://app.violencetrack.ng" />
+        <meta name="twitter:title" content="Election Violence Tracker" />
+        <meta
+          name="twitter:description"
+          content="Report and share Violence Incidence directly on this platform."
+        />
+        <meta
+          name="twitter:image"
+          content="https://app.violencetrack.ng/icon-512x512.png"
+        />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Election Violence Tracker" />
+        <meta
+          property="og:description"
+          content="Report and share Violence Incidence directly on this platform."
+        />
+        <meta property="og:site_name" content="Election Violence Tracker" />
+        <meta property="og:url" content="https://app.violencetrack.ng" />
+        <meta
+          property="og:image"
+          content="https://app.violencetrack.ng/icon-512x512.png"
+        />
       </Head>
 
       <Box>
